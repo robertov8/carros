c45b484166ec4758e15391d87c46284a2487f8b2
Roberto Ribeiro <robertov82008@gmail.com>
2018-01-25 19:54:34 -0200

    9.3 Classe Application - Armazenando informações globais

---
+++ app/src/main/java/br/com/livroandroid/carros/CarrosApplication.kt
@@ -0,0 +1,33 @@
+package br.com.livroandroid.carros
+
+import android.app.Application
+import android.util.Log
+
+class CarrosApplication : Application() {
+    private val TAG = "CarrosApplication"
+
+    // Chamado quando o Android cria o processo de aplicação
+    override fun onCreate() {
+        super.onCreate()
+
+        // Salva a instância para termos acesso como Singleton
+        appInstance = this
+    }
+
+    companion object {
+        // Singleton da classe Application
+        private var appInstance: CarrosApplication? = null
+        fun getInstance(): CarrosApplication {
+            if (appInstance == null) {
+                throw IllegalStateException("Configure a classe de Application no AndroidManifest.xml")
+            }
+
+            return appInstance!!
+        }
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        Log.d(TAG, "CarrosApplication.onTerminate()")
+    }
+}
