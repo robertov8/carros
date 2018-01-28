dfb6099295a01e0b19332d83ef1c59b2d528772a
Roberto Ribeiro <robertov82008@gmail.com>
2018-01-28 02:29:32 -0200

    12.6 Adeus, findViewById(id), diga olá ao Kotlin Extensions

--- app/src/main/java/br/com/livroandroid/carros/activity/SiteLivroActivity.kt
+++ app/src/main/java/br/com/livroandroid/carros/activity/SiteLivroActivity.kt
@@ -2,20 +2,16 @@ package br.com.livroandroid.carros.activity
 
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.support.v4.widget.SwipeRefreshLayout
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.ProgressBar
 import br.com.livroandroid.carros.R
 import br.com.livroandroid.carros.activity.dialogs.AboutDialog
 import br.com.livroandroid.carros.extensions.setupToolbar
+import kotlinx.android.synthetic.main.activity_site_livro.*
 
 class SiteLivroActivity : BaseActivity() {
     private val URL_SOBRE = "http://www.livroandroid.com.br/sobre.htm"
-    lateinit var webview: WebView
-    lateinit var progress: ProgressBar
-    lateinit var swipeToRefresh: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,16 +21,11 @@ class SiteLivroActivity : BaseActivity() {
         val actionBar = setupToolbar(R.id.toolbar, "Site do Livro")
         actionBar.setDisplayHomeAsUpEnabled(true)
 
-        // Views
-        webview = findViewById<WebView>(R.id.webview)
-        progress = findViewById<ProgressBar>(R.id.progress)
-
         // Carrega a página
         setWebViewClient(webview)
         webview.loadUrl(URL_SOBRE)
 
         // Swipe to Refresh
-        swipeToRefresh = findViewById<SwipeRefreshLayout>(R.id.swipeToRefresh)
         swipeToRefresh.setOnRefreshListener { webview.reload() }
 
         // Cores de animação
