72a41e9a8b93c13d36898a77f23da57a790201d6
Roberto Ribeiro <robertov82008@gmail.com>
2018-01-29 18:06:59 -0200

    13.4 Melhorando o código do adapter - graças ao Kotlin 13.5 Função with()

--- app/src/main/java/br/com/livroandroid/carros/adapter/CarroAdapter.kt
+++ app/src/main/java/br/com/livroandroid/carros/adapter/CarroAdapter.kt
@@ -1,17 +1,13 @@
 package br.com.livroandroid.carros.adapter
 
-import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
 import br.com.livroandroid.carros.R
 import br.com.livroandroid.carros.domain.Carro
-import com.squareup.picasso.Callback
-import com.squareup.picasso.Picasso
+import br.com.livroandroid.carros.extensions.loadUrl
+import kotlinx.android.synthetic.main.adapter_carro.view.*
 
 // Define o construtor que recebe (carros, onClick)
 class CarroAdapter (
@@ -21,10 +17,10 @@ class CarroAdapter (
     // ViewHolder com as views
     class CarrosViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         // Salva as views no ViewHolder
-        var tNome: TextView = view.findViewById(R.id.tNome)
-        var img: ImageView = view.findViewById(R.id.img)
-        var progress: ProgressBar = view.findViewById(R.id.progress)
-        var cardView: CardView = view.findViewById(R.id.card_view)
+        //var tNome: TextView = view.findViewById(R.id.tNome)
+        //var img: ImageView = view.findViewById(R.id.img)
+        //var progress: ProgressBar = view.findViewById(R.id.progress)
+        //var cardView: CardView = view.findViewById(R.id.card_view)
     }
 
     // Retorna a quantidade de carros na lista
@@ -42,29 +38,23 @@ class CarroAdapter (
     }
 
     // Faz o bind para atualizar o valor das views com os dados do Carro
-    override fun onBindViewHolder(holder: CarrosViewHolder?, position: Int) {
-        val context = holder?.itemView?.context
-
+    override fun onBindViewHolder(holder: CarrosViewHolder, position: Int) {
         // Recupera o objeto carro
         val carro = carros[position]
 
-        // Atualiza os dados do carro
-        holder?.tNome?.text = carro.nome
-        holder?.progress?.visibility = View.VISIBLE
+        // Declara a variável view para facilitar o acesso abaixo
+        // A view contém as variáveis definidas no XML (lembre do nome de cada id)
+        val view = holder.itemView
 
-        // Faz o download da foto e mostra o ProgressBar
-        Picasso.with(context).load(carro.urlFoto).fit().into(holder?.img, object : Callback {
-            override fun onSuccess() {
-                // Download OK
-                holder?.progress?.visibility = View.GONE
-            }
+        with(view) {
+            // Atualiza os dados do carro
+            tNome.text = carro.nome
 
-            override fun onError() {
-                holder?.progress?.visibility = View.GONE
-            }
-        })
+            // Faz download da foto e mostra o ProgressBar
+            img.loadUrl(carro.urlFoto, view.progress)
 
-        // Adiciona o evento de clique na linha
-        holder?.itemView?.setOnClickListener { onClick(carro) }
+            // Adiciona o evento de clique na linha
+            setOnClickListener { onClick(carro) }
+        }
     }
 }
