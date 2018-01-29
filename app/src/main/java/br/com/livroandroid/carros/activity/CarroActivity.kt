aed835db97829ea918f377e8adbe9cbb6925ef04
Roberto Ribeiro <robertov82008@gmail.com>
2018-01-29 19:36:01 -0200

    13.7 Função lazy()

--- app/src/main/java/br/com/livroandroid/carros/activity/CarroActivity.kt
+++ app/src/main/java/br/com/livroandroid/carros/activity/CarroActivity.kt
@@ -8,23 +8,21 @@ import br.com.livroandroid.carros.extensions.setupToolbar
 import kotlinx.android.synthetic.main.activity_carro_contents.*
 
 class CarroActivity : BaseActivity() {
-    var carro: Carro? = null
+    val carro by lazy { intent.getSerializableExtra("carro") as Carro}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carro)
 
-        // Lê o carro enviado como parâmetro
-        carro = intent.getSerializableExtra("carro") as Carro
         // Seta o nome do carro como título na ToolBar
-        setupToolbar(R.id.toolbar, carro?.nome, true)
+        setupToolbar(R.id.toolbar, carro.nome, true)
         // Atualiza os dados do carro na tela
         initViews()
     }
 
     private fun initViews() {
         // Variáveis geradas automaticamente pelo Kotlin Extensions (veja import)
-        tDesc.text = carro?.desc
-        img.loadUrl(carro?.urlFoto)
+        tDesc.text = carro.desc
+        img.loadUrl(carro.urlFoto)
     }
 }
