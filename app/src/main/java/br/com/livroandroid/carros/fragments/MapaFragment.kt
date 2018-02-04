2af0ba813480a8e0e53d8cca956e8bd974c2d5b6
Roberto Ribeiro <robertov82008@gmail.com>
2018-02-04 14:57:15 -0200

    20.7 Mostrando a localização do usuário no mapa

--- app/src/main/java/br/com/livroandroid/carros/fragments/MapaFragment.kt
+++ app/src/main/java/br/com/livroandroid/carros/fragments/MapaFragment.kt
@@ -1,11 +1,15 @@
 package br.com.livroandroid.carros.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.livroandroid.carros.R
 import br.com.livroandroid.carros.domain.Carro
+import br.com.livroandroid.carros.utils.PermissionUtils
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -27,20 +31,47 @@ class MapaFragment : BaseFragment(), OnMapReadyCallback {
         return view
     }
 
-    // o método onMapReady(map) é chamado quando a inicialização do mapa estiver Ok
+    @SuppressLint("MissingPermission")
     override fun onMapReady(map: GoogleMap) {
+        // o método onMapReady(map) é chamado quando a inicialização do mapa estiver Ok
         this.map = map
-        // Cria o objeto lat/lng com a coordenada da fábrica
-        val location = LatLng(carro.latitude.toDouble(), carro.longitude.toDouble())
-        // Posiciona o mapa na coordenada da fábrica (zoom = 13)
-        val update = CameraUpdateFactory.newLatLngZoom(location, 13f)
-        map.moveCamera(update)
-        // Marcador no local da fábrica
-        map.addMarker(MarkerOptions()
-                .title(carro.nome)
-                .snippet(carro.desc)
-                .position(location))
+        // Vamos mostrar a localização do usuário apenas para carros com lat/lng=0
+        if (carro.latitude.toDouble() == 0.0) {
+            // Ativa o botão para mostrar minha localização
+            val ok = PermissionUtils.validate(activity, 1,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+            if (ok) {
+                // Somente usa o GPS se a permissão estiver OK.
+                map.isMyLocationEnabled = true
+            }
+        } else {
+            // Cria o objeto lat/lng com a coordenada da fábrica
+            val location = LatLng(carro.latitude.toDouble(), carro.longitude.toDouble())
+            // Posiciona o mapa na coordenada da fábrica (zoom = 13)
+            val update = CameraUpdateFactory.newLatLngZoom(location, 13f)
+            map.moveCamera(update)
+            // Marcador no local da fábrica
+            map.addMarker(MarkerOptions()
+                    .title(carro.nome)
+                    .snippet(carro.desc)
+                    .position(location))
+        }
+
         // Tipo do mapa: normal, satélite, terreno ou híbrido
         map.mapType = GoogleMap.MAP_TYPE_NORMAL
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        for (result in grantResults) {
+            if (result == PackageManager.PERMISSION_GRANTED) {
+                // Permissão OK, podemos usar o GPS.
+                map?.isMyLocationEnabled = true
+                return
+            }
+        }
     }
 }
